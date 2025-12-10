151a7818e9d5a1842c37e85d4e4a7bea88f7b969
Borut Tomazin <borut@povio.com>
2025-12-10 14:12:29 +0100

    Public method.

--- Sources/Core/Models/Nonce.swift
+++ Sources/Core/Models/Nonce.swift
@@ -17,15 +17,13 @@ public extension Nonce {
 	var value: String {
 		switch self {
 		case .random(let length):
-			return generateNonceString(length: length).sha256
+			return Self.generateNonceString(length: length).sha256
 		case .custom(let value):
 			return value.sha256
 		}
 	}
-}
-
-private extension Nonce {
-	func generateNonceString(length: UInt = 32) -> String {
+	
+	static func generateNonceString(length: UInt = 32) -> String {
 		guard length > 0 else { return "zero-length-nonce" }
 		let charset: [Character] = Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
 		let result = (0..<length).compactMap { _ in charset.randomElement() }
